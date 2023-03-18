7f61267ca8bc1b51a4dda7470456cb93f3da5869
William Rodriguez <davidwdrg@gmail.com>
2023-03-18 12:24:16 -0500

    agregar páginación

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Search from "./components/Search"
 import ResidentInfo from "./components/ResidentInfo"
-import Pagination from "./components/Pagination"
 import Location from './components/Location'
 import Loader from './components/Loader'
+
 
 
 
@@ -13,7 +13,7 @@ function App() {
   const [fetchedData, SetFetchedData] = useState({})
   const [modal, setModal] = useState(false)
   const [loading, setLoading] = useState(true)
-  const [pageNumber, updatePageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1)
  
 
   const api = `https://rickandmortyapi.com/api/location/${Math.floor(Math.random()*126)+1}`
@@ -32,15 +32,20 @@ function App() {
         .catch(err => console.log(err))
   },[])
 
+  const itemsPerPage = 10
+
+  const startIndex = (pageNumber - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
 
   
   return (
     <div className="App">
       {loading && <Loader/>}
+
       <div className='background_img'>
         <img src="/fondo/Frame 432.png" alt="background image"/>
         <img src="/fondo/Rick y Morty2.png" alt="background image"/>
-
       </div>
     
       <div className='container_logo'>
@@ -57,12 +62,14 @@ function App() {
       <Location data = {fetchedData}/>
       </ul>
 
-  
+
     <div className="cardContainer">
-      {fetchedData.residents?.map(residentUrl => (
+      {fetchedData.residents?.slice(startIndex, endIndex).map((residentUrl) => (
       <ResidentInfo 
       key={residentUrl} 
-      url = {residentUrl}/>))}
+      url = {residentUrl}
+      />
+      ))}
     </div>
 
     <div className={modal ? "container__modal" : "close__modal"}>
@@ -78,8 +85,25 @@ function App() {
       </div>
     </div>
 
+          <div className="pagination">
+            <button 
+                disabled={pageNumber === 1} 
+                onClick={() => setPageNumber(pageNumber - 1)}
+                className="paginationItems"
 
-      {/* <Pagination/> */}
+            >
+                Anterior
+            </button>
+            <span className="pagItems"> Página {pageNumber}</span>
+            <button 
+                disabled={fetchedData.residents?.length <= endIndex} 
+                onClick={() => setPageNumber(pageNumber + 1)}
+                className="paginationItems"
+            >
+                Siguiente
+            </button>
+        </div> 
+
     </div>
   )
 }
